d201a9ae63d090d07ccbc190cdb5351894ef523b
Soso Pkhakadze <soso.pkhakadze.dev@gmail.com>
2025-10-21 21:52:06 +0400

    Updated n8n

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ const addTask = async (
     
     try {
       // Your actual N8N webhook URL
-      const response = await fetch('https://sosopkhakadze.app.n8n.cloud/webhook/06accbd7-96d9-4dbe-ad88-f6f5af121f14', {
+      const response = await fetch('https://soso-pkhakadze.app.n8n.cloud/webhook/0caa17ec-148e-4754-a38d-5cd803207428', {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
